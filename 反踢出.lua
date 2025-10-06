815b15e7948e71d29ba4b16de91eee7789e7302b
Abc123796 <1130785549@qq.com>
2025-10-07 01:55:27 +0800

    Add files via upload

---
+++ 反踢出.lua
@@ -0,0 +1,5 @@
+--在执行之后就不会被踢出了 切记不要宣传 想玩你就进霖溺群
+--制作者群聊744830231
+--会打印到控制台 聊天框输入/Console 有具体时间和踢出内容
+AntiKick = "反踢出"
+loadstring(game:HttpGet('https://github.com/XiaoyeQWQ/Anti-Kick/raw/refs/heads/main/Main-Kick-function-Source.lua', true))()
